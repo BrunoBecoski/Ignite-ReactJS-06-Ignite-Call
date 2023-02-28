412c3f76e1fedcaafa8efdd8fd71335be3b7ba09
Bruno Becoski <brunobecoski@gmail.com>
2023-02-28 17:18:36 -0300

    Estrutura visual da home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,14 +1,1 @@
-import Head from 'next/head'
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Ignite Call</title>
-      </Head>
-
-      <Heading as="h1">Hello World</Heading>
-    </>
-  )
-}
+export { default } from './home'
