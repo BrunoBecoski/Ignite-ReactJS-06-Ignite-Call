fcc828def1def9646854453b7f27acf49a88e826
Bruno Becoski <brunobecoski@gmail.com>
2023-03-26 12:38:41 -0300

    Rota de agendamento

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,9 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
 import { z } from 'zod'
 import dayjs from 'dayjs'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import { CalendarBlank, Clock } from 'phosphor-react'
+
+import { api } from '../../../../../lib/axios'
 
 import { ConfirmForm, FormHeader, FormError, FormActions } from './styles'
 
@@ -34,8 +37,20 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleConfirmScheduling(data: ConfirmFormData) {
-    console.log(data)
+  const router = useRouter()
+  const username = String(router.query.username)
+
+  async function handleConfirmScheduling(data: ConfirmFormData) {
+    const { name, email, observations } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
   }
 
   const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
